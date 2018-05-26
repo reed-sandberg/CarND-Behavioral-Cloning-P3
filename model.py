ef355634dbc04466d84db0b58569948cfb8b38e8
Reed Sandberg <reed.sandberg@drawbridge.com>
2018-05-25 22:54:47 -0700

    Behavior cloning.
    * Include resize.
    * Add dropouts.

--- model.py
+++ model.py
@@ -9,8 +9,9 @@ import cv2
 import numpy as np
 import random
 
+from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Cropping2D, Dense, Flatten, Lambda
+from keras.layers import Cropping2D, Dense, Dropout, Flatten, Lambda
 from keras.layers.convolutional import Convolution2D
 
 import sklearn
@@ -45,6 +46,8 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 name = './IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
+                #center_image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+                #center_image = center_image.reshape(160, 320, 1)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
@@ -60,17 +63,21 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 ch, row, col = 3, 66, 320  # Trimmed image format
 
-nb_epoch = 10
+nb_epoch = 1
+dropout_rate = 0.25
 
 model = Sequential()
 
 # set up cropping2D layer
 model.add(Cropping2D(cropping=((70, 24), (0, 0)), input_shape=(160, 320, 3)))
 
+model.add(Lambda(lambda image: K.tf.image.resize_images(image, (66, 200)),
+                 input_shape=(row, col, ch),
+                 output_shape=(66, 200, ch)))
 # Preprocess incoming data, centered around zero with small standard deviation
 model.add(Lambda(lambda x: x/127.5 - 1.,
-                 input_shape=(row, col, ch),
-                 output_shape=(row, col, ch)))
+                 input_shape=(66, 200, ch),
+                 output_shape=(66, 200, ch)))
 ####at need 66x200 from 320x160 -> 320x66
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
@@ -79,8 +86,11 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(dropout_rate))
 model.add(Dense(50))
+model.add(Dropout(dropout_rate))
 model.add(Dense(10))
+model.add(Dropout(dropout_rate))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
