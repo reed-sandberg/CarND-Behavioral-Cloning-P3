bd6a7e3e036b7bb6d11e9fc3b4eb87e16af6eb75
Reed Sandberg <reed.sandberg@drawbridge.com>
2018-05-27 22:59:56 -0700

    Behavioral cloning
    * Code cleanup, documentation.

--- model.py
+++ model.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-# Copyright (C) 2016 Leapfin All Rights Reserved
 """
-This script...
+CNN model to fit simulated autonomous driving via behavioral cloning.
 """
 
 import csv
-import cv2
-import numpy as np
 import random
+
+import numpy as np
 
 from keras import backend as K
 from keras.models import Sequential
@@ -17,26 +16,26 @@ from keras.layers.convolutional import Convolution2D
 import sklearn
 from sklearn.model_selection import train_test_split
 
-
-#image_flipped = np.fliplr(image)
-#measurement_flipped = -measurement
+import cv2
 
 
-#model.add(Lambda(lambda pix_chan: pix_chan / 255 - 0.5, input_shape=(160, 320, 3)))
+# Training data shape.
+TRAIN_DATA_WIDTH = 320
+TRAIN_DATA_HEIGHT = 160
 
-
-samples = []
-with open('./driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-train_samples, validation_samples = train_test_split(samples, test_size=0.02)
+# Hyper-parameters.
+NB_EPOCH = 1
+DROPOUT_RATE = 0.25
+CROP_TOP = 60
+CROP_BOTTOM = 20
+VALIDATION_SPLIT = 0.02
+BATCH_SIZE = 32
 
 def generator(samples, batch_size=32):
-    #import pdb; pdb.set_trace()
+    """Generate a stream of batched samples with batch_size."""
     num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
+    # Generator does not terminate, may use more than one epoch.
+    while True:
         random.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
@@ -45,58 +44,79 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 name = './IMG/'+batch_sample[0].split('/')[-1]
-                #center_image = cv2.imread(name)
+                # Read as grayscale but preserve 3-D structure with the single color channel last.
                 center_image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
-                center_image = center_image.reshape(160, 320, 1)
+                center_image = center_image.reshape(TRAIN_DATA_HEIGHT, TRAIN_DATA_WIDTH, 1)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+def main():
+    """Main entry point."""
+    samples = []
+    with open('./driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
 
-ch, row, col = 1, 80, 320  # Trimmed image format
+    train_samples, validation_samples = train_test_split(samples, test_size=VALIDATION_SPLIT)
 
-nb_epoch = 1
-dropout_rate = 0.25
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+    validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
 
-model = Sequential()
+    model = Sequential()
 
-# set up cropping2D layer
-model.add(Cropping2D(cropping=((60, 20), (0, 0)), input_shape=(160, 320, 1)))
+    # Crop training images to focus on the road.
+    model.add(Cropping2D(cropping=((CROP_TOP, CROP_BOTTOM),
+                                   (0, 0)),
+                         input_shape=(TRAIN_DATA_HEIGHT, TRAIN_DATA_WIDTH, 1)))
+    # Resize training images with a built-in scaling algo.
+    model.add(Lambda(lambda image: K.tf.image.resize_images(image, (66, 200)),
+                     input_shape=(TRAIN_DATA_HEIGHT - CROP_TOP - CROP_BOTTOM, TRAIN_DATA_WIDTH, 1),
+                     output_shape=(66, 200, 1)))
+    # Normalize training data centered around zero with a small standard deviation.
+    model.add(Lambda(lambda x: x/127.5 - 1.))
 
-model.add(Lambda(lambda image: K.tf.image.resize_images(image, (66, 200)),
-                 input_shape=(row, col, ch),
-                 output_shape=(66, 200, ch)))
-# Preprocess incoming data, centered around zero with small standard deviation
-model.add(Lambda(lambda x: x/127.5 - 1.,
-                 input_shape=(66, 200, ch),
-                 output_shape=(66, 200, ch)))
-####at need 66x200 from 320x160 -> 320x66
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dropout(dropout_rate))
-model.add(Dense(50))
-model.add(Dropout(dropout_rate))
-model.add(Dense(10))
-model.add(Dropout(dropout_rate))
-model.add(Dense(1))
+    # CNN model based on NVIDIA publication with 5 convolutional layers followed by 3 fully-connected layers.
+    # https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+    #
+    # Image input 66x200 (1 channel) -> 31x98 @24 deep
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    # 14x47 @36 deep
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    # 5x22 @48 deep
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    # 3x20 @64 deep
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    # 1x18 @64 deep
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Flatten())
+    # Fully-connected 1164 -> 100
+    model.add(Dense(100))
+    model.add(Dropout(DROPOUT_RATE))
+    # Fully-connected 50
+    model.add(Dense(50))
+    model.add(Dropout(DROPOUT_RATE))
+    # Fully-connected 10
+    model.add(Dense(10))
+    model.add(Dropout(DROPOUT_RATE))
+    # Output
+    model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
-                    validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples),
-                    nb_epoch=nb_epoch)
+    # Fit the model using Adam to optimize MSE loss.
+    model.compile(loss='mse', optimizer='adam')
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
+                        validation_data=validation_generator,
+                        nb_val_samples=len(validation_samples),
+                        nb_epoch=NB_EPOCH)
 
-model.save('trained_model.h5')
+    # Save optimized model.
+    model.save('model.h5')
+
+if __name__ == '__main__':
+    main()
